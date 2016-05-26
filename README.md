4f06730e1fc615cd0d8896f08f26fa191eaceaff
Bartosz Wiśniewski <poprosturonin@gmail.com>
2016-05-26 23:21:20 +0200

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# cookies
-Good plain JS cookie alert
+# Cookies
+=========
+Good, plain JS cookie alert. Saves cookie with name 'cookieAlert' when user closes alert.
+Doesn't require any frameworks to work.
+
+Use
+------------
+`<script src="https://poprosturonin.github.io/cookies/cookies.js"></script>`
+Include it, nothing more.
+
+Demo
+------------
+[Click here](https://www.poprosturonin.github.io/cookies)
+
+License
+------------
+Use it anywhere you want, incluing commercial projects. You can edit it to match your expectation.
+You cannot remove credits from cookies.js file.
+
+close.png comes from [Google Material Icons](https://design.google.com/icons/) set and it licensed under [CC-BY](https://creativecommons.org/licenses/by/4.0/)
+
+Contributing
+------------
+Just do it, but one thing:
+Submissions must be licensed under the MIT License.
---
+++ cookies.js
@@ -0,0 +1,77 @@
+/*
+Cookie alert
+Original repository: https://github.com/PoprostuRonin/cookies
+http://poprosturonin.com
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  /* Check if user has seen this alert before */
+  var hasSeen = false;
+  var cookies = document.cookie.split(';');
+  for(var i = 0; i < cookies.length; i++) {
+      var c = cookies[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf("cookieAlert=") == 0) {
+          hasSeen = true;
+          break;
+      }
+  }
+
+  if(hasSeen)
+    return false;
+
+  var container = document.createElement('div');
+  var link = document.createElement('a');
+  var image = document.createElement('img');
+
+  var containerStyle = 'font-size: 13px; z-index: 1000; width: 100%; box-sizing: border-box; padding: 5px 10px; opacity: 0.8; position: fixed; bottom: 0; background-color: #000; color: #FFF;'
+  var linkStyle = 'position: absolute; right: 10px; top: 5px; cursor: pointer;';
+
+  image.setAttribute('src','https://poprosturonin.github.io/cookies/close.png')
+  container.setAttribute('id', 'cookieinfo');
+  container.setAttribute('class', 'cookie-alert cookie-info cookies'); //Let filters do their job
+  container.setAttribute('style', containerStyle);
+  link.setAttribute('style', linkStyle);
+
+  container.innerHTML = '<span style="font-size: 11px;">This website use cookies</span><br>' +
+  'This website uses cookies to enchance your experience. You can change cookie settings in your browser.';
+
+  link.appendChild(image);
+  container.appendChild(link);
+
+  /* Callback */
+  function clickHandler(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    } else {
+        e.returnValue = false;
+    }
+
+    /* Create cookie that user saw the alert */
+    var date = new Date();
+    date.setTime(date.getTime() + (3*360*24*60*60*1000)); //3 years
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = "cookieAlert" + "=" + "seen" + "; " + expires;
+
+    /* Animation for alert */
+    var interval = window.setInterval(function() {
+        container.style.opacity -= 0.01;
+
+        if (container.style.opacity <= 0.02) {
+            document.body.removeChild(container);
+            window.clearInterval(interval);
+        }
+    }, 4);
+  }
+
+  /* Container is a big button, but link pretends to be one */
+  if (container.addEventListener) {
+      container.addEventListener('click', clickHandler);
+  } else {
+      container.attachEvent('onclick', clickHandler);
+  }
+
+  document.body.appendChild(container);
+});
